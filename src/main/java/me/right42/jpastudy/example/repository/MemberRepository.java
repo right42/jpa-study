3240e996861222a65912b7319fc8432cf8c3c965
right42 <chjm452@naver.com>
2020-12-29 23:18:36 +0900

    log spy apply

---
+++ src/main/java/me/right42/jpastudy/example/repository/MemberRepository.java
@@ -0,0 +1,25 @@
+package me.right42.jpastudy.example.repository;
+
+import me.right42.jpastudy.example.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Long save(Member member) {
+        entityManager.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+
+
+}
