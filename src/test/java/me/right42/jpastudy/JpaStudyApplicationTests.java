ee6c0f631f0293ab97d21835edd1d35f59262cb2
right42 <chjm452@naver.com>
2020-12-29 22:13:21 +0900

    init

---
+++ src/test/java/me/right42/jpastudy/JpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.right42.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
