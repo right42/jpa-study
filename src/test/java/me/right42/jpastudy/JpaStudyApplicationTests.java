342b5f48a7a7dc8b4e0bf53401fb9c8a81f91034
right42 <chjm452@naver.com>
2021-01-12 23:29:35 +0900

    엔티티 설계시 주의사항

--- src/test/java/me/right42/jpastudy/JpaStudyApplicationTests.java
+++ src/test/java/me/right42/jpastudy/JpaStudyApplicationTests.java
@@ -1,13 +1,28 @@
 package me.right42.jpastudy;
 
+import me.right42.jpastudy.shop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @SpringBootTest
 class JpaStudyApplicationTests {
 
+    @PersistenceContext
+    EntityManager entityManager;
+
     @Test
-    void contextLoads() {
+    @Transactional
+    void hibernateCollection() {
+        Member member = new Member();
+
+        System.out.println(member.getOrders().getClass());
+        entityManager.persist(member);
+        System.out.println(member.getOrders().getClass());
+
     }
 
 }
