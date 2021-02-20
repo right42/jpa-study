536f2661ab66e788d3f2ff3fd270c8d5082ba2ea
right42 <chjm452@naver.com>
2021-02-20 23:29:33 +0900

    itemService test code remove

---
+++ src/test/java/me/right42/jpastudy/shop/service/OrderServiceTest.java
@@ -0,0 +1,20 @@
+package me.right42.jpastudy.shop.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void orderTest() {
+
+    }
+
+}
